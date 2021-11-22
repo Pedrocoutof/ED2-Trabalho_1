a57f0d41b3b085b5d3f675a0708077b96317f1d9
Pedro do Couto Filgueiras <61303443+Pedrocoutof@users.noreply.github.com>
2021-11-22 13:44:24 -0300

    Detalhamento das Funções

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 
 using namespace std;
 
-#define NUM_MAX_DE_REGISTRO 400
+#define NUM_MAX_DE_REGISTRO 400 // TAMANHO DE LINHAS DO ARQUIVO '.csv' QUE USAMOS PARA TESTES //
 
+// ESTRUTURA USADA PARA ARMAZENAR DE FORMA SEPARADA CADA INFORMAÇÃO DO ARQUIVO '.csv' //
 typedef struct{
 
     char review_id [90];        //  string identificadora única
@@ -18,20 +19,12 @@ typedef struct{
 
 }TiktokReviews;
 
+// FUNCAO USADA PARA GERAR E RETORNAR VALOR ALEATORIO DE FORMA MAIS PRÁTICA E LEGÍVEL //
 int valorAleatorio(){
     return rand() % NUM_MAX_DE_REGISTRO;
 }
 
-void printaArquivoTxt(FILE * arq){
-    for(char c = fgetc(arq); !feof(arq); c = fgetc(arq)){
-        printf("%c", c);
-        if(c == '\n'){
-            cout << endl;
-        }
-    }
-    rewind(arq);
-}
-
+// FUNCAO USADA PARA IMPRIMIR NO CONSOLE CONTEÚDO DE UM REGISTRO GUARDADO NA ESTRUTURA 'TiktokReviews' //
 void printaConteudoTiktokReviews(TiktokReviews tiktokReviews){
 
     cout <<     "Review ID : "       <<      tiktokReviews.review_id      << endl;
@@ -42,28 +35,27 @@ void printaConteudoTiktokReviews(TiktokReviews tiktokReviews){
 
 }
 
-void gravaTiktokReviewEmBinario(FILE * arq_csv, FILE * arq_bin){
+// FUNÇÃO USADA PARA LER TODO ARQUIVO '.csv' E GERAR UM ARQUIVO '.bin' //
+void gravaCsvEmBinario(FILE * arq_csv, FILE * arq_bin){
 
-    rewind(arq_csv);
-    rewind(arq_bin);
+    rewind(arq_csv);    // REINICIA O PONTEIRO DO '.csv' //
+    rewind(arq_bin);    // REINICIA O PONTEIRO DO '.bin' //
     char c;
 
-    // PULA A PRIMEIRA LINHA DO "arq_csv" //
-    for(c = fgetc(arq_csv); c != '\n'; c = fgetc(arq_csv)){}
-
+    for(c = fgetc(arq_csv); c != '\n'; c = fgetc(arq_csv)){}    // PULA A PRIMEIRA LINHA DO '.csv' JÁ QUE A PRIMEIRA LINHA TEM INFORMAÇÕES DE COMO O ARQUIVO É SEPARADO //
     while(!feof(arq_csv)){
 
         TiktokReviews tiktokReviews;
 
-        fscanf(arq_csv, "%[^,]", tiktokReviews.review_id);
+        fscanf(arq_csv, "%[^,]", tiktokReviews.review_id);  // FAZ A LEITURA ATÉ A VIRGULA E GUARDA NA VARIAVEL 'tiktokReviews.review_id' //
         fgetc(arq_csv);
 
         c = fgetc(arq_csv);
-        if(c == '"'){
+        if(c == '"'){       // VERIFICA SE O REVIEW_TEXT DO ARQUIVO '.csv' É SEPARADO POR ' " ' //
                 int i = 0;
                 c = fgetc(arq_csv);
             while(c!='"'){
-                tiktokReviews.review_text[i] = c;
+                tiktokReviews.review_text[i] = c;   // SE FOR ELE IRÁ GUARDAR ATÉ A PRÓXIMA ' " ' //
                 c = fgetc(arq_csv);
                 i++;
             }
@@ -74,7 +66,7 @@ void gravaTiktokReviewEmBinario(FILE * arq_csv, FILE * arq_bin){
                 fseek(arq_csv, -1, SEEK_CUR);
                 int i = 0;
                 c = fgetc(arq_csv);
-                while(c != ','){
+                while(c != ','){    // CASO CONTRÁRIO ELE GUARDA ATÉ A PRÓXIMA ',' //
                     tiktokReviews.review_text[i] = c;
                     c = fgetc(arq_csv);
                     i++;
@@ -82,14 +74,14 @@ void gravaTiktokReviewEmBinario(FILE * arq_csv, FILE * arq_bin){
             tiktokReviews.review_text[i] = '\0';
         }
 
-        fscanf(arq_csv, "%[^,]", tiktokReviews.upvotes);
+        fscanf(arq_csv, "%[^,]", tiktokReviews.upvotes);    // GUARDA UPVOTES EM 'tiktokReviews.upvotes' ATÉ A PRÓXIMA VIRGULA //
         fgetc(arq_csv);
 
-        fscanf(arq_csv, "%[^,]", tiktokReviews.app_version);
+        fscanf(arq_csv, "%[^,]", tiktokReviews.app_version);    // GUARDA APP_VERSION NA VARIAVEL 'tiktokReviews.app_version' ATE A PRÓXIMA VIRGULA //
         fgetc(arq_csv);
 
         int i = 0;
-        for(c = fgetc(arq_csv); c != '\n' && c!=EOF; c = fgetc(arq_csv)){
+        for(c = fgetc(arq_csv); c != '\n' && c!=EOF; c = fgetc(arq_csv)){   // GUARDA O RESTO DO CONTEUDO VERIFICANDO SE CHEGOU NO FINAL DA LINHA '\n' OU SE CHEGOU NO FINAL DO ARQUIVO 'EOF' E GUARDA NA VARIAVEL 'tiktokReviews.posted_date' //
             tiktokReviews.posted_date[i] = c;
             i++;
         }
@@ -97,14 +89,15 @@ void gravaTiktokReviewEmBinario(FILE * arq_csv, FILE * arq_bin){
 
         //printaConteudoTiktokReviews(tiktokReviews);
 
-        fwrite(tiktokReviews.review_id,     sizeof(tiktokReviews.review_id),    1, arq_bin);
-        fwrite(tiktokReviews.review_text,   sizeof(tiktokReviews.review_text),  1, arq_bin);
-        fwrite(&tiktokReviews.upvotes,      sizeof(tiktokReviews.upvotes),      1, arq_bin);
-        fwrite(tiktokReviews.app_version,   sizeof(tiktokReviews.app_version),  1, arq_bin);
-        fwrite(tiktokReviews.posted_date,   sizeof(tiktokReviews.posted_date),  1, arq_bin);
+        fwrite(tiktokReviews.review_id,     sizeof(tiktokReviews.review_id),    1, arq_bin);    // GRAVA EM BINARIO O 'review_id'   // 
+        fwrite(tiktokReviews.review_text,   sizeof(tiktokReviews.review_text),  1, arq_bin);    // GRAVA EM BINARIO O 'review_text' //
+        fwrite(&tiktokReviews.upvotes,      sizeof(tiktokReviews.upvotes),      1, arq_bin);    // GRAVA EM BINARIO O 'upvotes'     //
+        fwrite(tiktokReviews.app_version,   sizeof(tiktokReviews.app_version),  1, arq_bin);    // GRAVA EM BINÁRIO O 'app_version' //
+        fwrite(tiktokReviews.posted_date,   sizeof(tiktokReviews.posted_date),  1, arq_bin);    // GRAVA EM BINÁRIO O 'posted_date' //
     }
 }
 
+// FUNÇÃO USADA PARA ACESSAR REGISTRO E IMPRIMIR NO CONSOLE O VALOR DO REGISTRO PASSADO POR PARAMETRO //
 void acessaRegistro(int i){
 
     FILE * arq_bin = fopen("tiktok_app_reviews.bin", "rb");
@@ -137,6 +130,7 @@ void acessaRegistro(int i){
     fclose(arq_bin);
 }
 
+// FUNCAO SEMELHANTE A 'acessaRegistro' MAS QUE AO INVÉS DE PRINTAR NO CONSOLE ELA RETORANAR O VALOR DO REGISTRO PASSADO POR PARAMETRO //
 TiktokReviews retornaRegistro(int i){
 
     FILE * arq_bin = fopen("tiktok_app_reviews.bin", "rb");
@@ -162,6 +156,7 @@ TiktokReviews retornaRegistro(int i){
     return tiktokReviews;
 }
 
+// FUNCAO UTILIZADA PARA GRAVAR EM UM ARQUIVO DE TEXTO, TODAS VARIAVEIS DO TIPO 'TiktokReviews' //
 void gravaTiktokReviewEmTxt(FILE * arq_txt, TiktokReviews tiktokReviews){
 
             fprintf(arq_txt, "%s", tiktokReviews.review_id);
@@ -176,40 +171,9 @@ void gravaTiktokReviewEmTxt(FILE * arq_txt, TiktokReviews tiktokReviews){
             fprintf(arq_txt, "%c", '\n');
 }
 
-void gravaLinhaEmBinario(FILE * arq_txt, FILE * arq_bin){
-
-    char linha[500];
-    char c;
-    int i = 0;
-    while(!feof(arq_txt)){
-        c = fgetc(arq_txt);
-        if(c != '\n'){
-            linha[i] = c;
-            i++;
-            linha[i+1] = '\0';
-        }
-        else{
-            fwrite(linha, sizeof(linha), 1, arq_bin);
-            i = 0;
-            linha[0] = '\0';
-            }
-        }
-
-}
-
-void leLinhaEmBinario(int i){
-    FILE * arq_bin = fopen("linha.bin", "rb");
-    char linha[500];
-
-    fseek(arq_bin, i * sizeof(linha), 1);
-    fread(&linha, sizeof(linha), 1, arq_bin);
-    cout << linha << endl;
-
-}
-
-int menuTesteImportacao(){
-
-    printf("%c", 201);
+// FUNCAO QUE GERA NO CONSOLE UM MENU PARA INTERAÇÃO COM O USUARIO PARA TESTAR A IMPORTAÇÃO //
+void menuTesteImportacao(){
+printf("%c", 201);
     for(int i = 0; i < 55; i ++){
         printf("%c", 461);
     }
@@ -242,12 +206,34 @@ int menuTesteImportacao(){
     }
     printf("%c\n", 188);
 
+}
+
+// FUNCAO PARA TESTAR A IMPORTACAO //
+void testaImportacao(){
+
     int resp;
     scanf("%d", &resp);
-    return resp;
+    
+        srand(time(nullptr));
+
+    if(resp == 1){
+        for(int i = 0; i < 10; i++){
+                acessaRegistro(valorAleatorio());
+        }
+    }
+
+    else if(resp == 2){
+        FILE * arq_txt = fopen("registrosAleatorios.txt", "w");
+        for(int i = 0; i < 100; i++){
+            TiktokReviews tiktokReviews = retornaRegistro(valorAleatorio());
+            gravaTiktokReviewEmTxt(arq_txt, tiktokReviews);
+        }
+    }
+
 
 }
 
+// FUNCAO QUE GERA NO CONSOLE UM MENU PARA INTERAÇÃO COM O USUARIO PARA TESTAR A FUNÇÃO 'acessaRegistro' //
 int menuAcessaRegistro(){
 
     printf("%c", 201);
@@ -275,9 +261,9 @@ int menuAcessaRegistro(){
 
 int main(int argc, char * argv[])
 {
-    //char diretorio_csv[] = "H:\\UFJF\\2021.3\\ED2\\Trabalho 1\\tiktok_app_reviews_reduzido.csv";
-    //FILE * arq_csv = fopen(diretorio_csv, "r");
-    //H:\UFJF\2021.3\ED2\Trabalho 1\tiktok_app_reviews_reduzido.csv
+    // char diretorio_csv[] = "H:\\UFJF\\2021.3\\ED2\\Trabalho 1\\tiktok_app_reviews_reduzido.csv";
+    // FILE * arq_csv = fopen(diretorio_csv, "r");
+    // H:\UFJF\2021.3\ED2\Trabalho 1\tiktok_app_reviews_reduzido.csv
 
     FILE * arq_csv = fopen(argv[1], "r");
 
@@ -293,7 +279,7 @@ int main(int argc, char * argv[])
         cout << endl << "Arquivo <" << diretorio_bin << "> nao encontrado." << endl;
         cout << "Gerando <" << diretorio_bin << ">." << endl;
         FILE * arq_bin = fopen(diretorio_bin, "wb");
-        gravaTiktokReviewEmBinario(arq_csv, arq_bin);
+        gravaCsvEmBinario(arq_csv, arq_bin);
         fclose(arq_bin);
     }
 
@@ -301,22 +287,9 @@ int main(int argc, char * argv[])
 
     acessaRegistro(resp);
 
-    resp = menuTesteImportacao();
-        srand(time(nullptr));
+    menuTesteImportacao();
 
-    if(resp == 1){
-        for(int i = 0; i < 10; i++){
-                acessaRegistro(valorAleatorio());
-        }
-    }
-
-    else if(resp == 2){
-        FILE * arq_txt = fopen("registrosAleatorios.txt", "w");
-        for(int i = 0; i < 100; i++){
-            TiktokReviews tiktokReviews = retornaRegistro(valorAleatorio());
-            gravaTiktokReviewEmTxt(arq_txt, tiktokReviews);
-        }
-    }
+    testaImportacao();
 
     cout << endl << endl << endl << "Pressione qualquer tecla para sair...";
     getch();
